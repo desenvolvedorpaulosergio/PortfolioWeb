95ef0a9aa294dc98d9128962be62c9e8c9ad32a4
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-09-26 13:35:38 -0300

    Atualizar

--- classjs/index.js
+++ classjs/index.js
@@ -10,9 +10,13 @@ function eventiEquiv(){
         setTimeout(function(){
             window.document.getElementById('config-display-opcoes-ocultas').style.display = "none"
         },200)
+        window.document.getElementById('config-opcoes-curriculo').style.display = "none"
+        window.document.getElementById('config-opcoes-projetos').style.display = "none"
+        window.document.getElementById('config-opcoes-sobre').style.display = "none"
     }else{
         window.document.getElementById('config-display-opcoes-ocultas').style.display = "block"
         window.document.getElementById('config-display-opcoes-ocultas').style.animationName = "anicacaoOpcoesOcultas"
+        window.document.getElementById('config-sobree').style.color = "white"
     }
 }
 
@@ -31,5 +35,13 @@ function eventoProjetos(){
         window.document.getElementById('config-opcoes-projetos').style.display = "none"
     }else{
         window.document.getElementById('config-opcoes-projetos').style.display = "block"
+    }
+}
+
+function eventoSobre(){
+    if(window.document.getElementById('config-opcoes-sobre').style.display == "block"){
+        window.document.getElementById('config-opcoes-sobre').style.display = "none"
+    }else{
+        window.document.getElementById('config-opcoes-sobre').style.display = "block"
     }
 }
