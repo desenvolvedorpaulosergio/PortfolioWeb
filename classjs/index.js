ee833df15c2c0f7e58d0e5d58bb4d0ede9dd889c
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-09-26 12:41:32 -0300

    Atualizar progresso

--- classjs/index.js
+++ classjs/index.js
@@ -1,3 +1,9 @@
+function comprimentoBody(){
+    if(window.innerWidth > 550){
+        window.document.getElementById('config-display-opcoes-ocultas').style.display = "none"
+    }
+}
+
 function eventiEquiv(){
     if(window.document.getElementById('config-display-opcoes-ocultas').style.display == "block"){
         window.document.getElementById('config-display-opcoes-ocultas').style.animationName = "anicacaoOpcoesOcultas2"
@@ -11,5 +17,9 @@ function eventiEquiv(){
 }
 
 function eventoCurriculo(){
-
+    if(window.document.querySelector('#config-opcoes-curriculo').style.display == "block"){
+        window.document.querySelector('#config-opcoes-curriculo').style.display = "none"
+    }else{
+        window.document.querySelector('#config-opcoes-curriculo').style.display = "block"
+    }
 }
